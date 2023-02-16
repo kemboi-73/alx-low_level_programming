055cbd4ff361668a9c84f8c492ddc25b3bbbe1b1
kemboi-73 <kemboimichael52@gmail.com>
2023-02-16 09:33:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
- /**
-  * main - Start point
-  *
-  * description: 'a program that prints followed by a line'
-  *
-  * Return: 0 (Success)
-  */
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints a message to the console followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
