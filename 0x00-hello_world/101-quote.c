ca435e209a89caee8dd6d42ec2a3e72ebfa1c2f3
kemboi-73 <kemboimichael52@gmail.com>
2023-02-16 12:08:48 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,7 @@
 
 int main(void)
 {
-char *message = "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n";
-write(STDERR_FILENO, message, 59);
+fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
 
