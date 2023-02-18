cf76a81f0c55f71362fa24ddea45f92dbd184197
kemboi-73 <kemboimichael52@gmail.com>
2023-02-18 02:06:30 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,21 +9,28 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int i, j;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-putchar((i == 9 && j == 9) ? '\n' : ',');
-putchar((i == 9 && j == 9) ? '\0' : ' ');
+if (i == 8 && j == 9)
+{
+putchar('\n');
+return (0);
+}
+putchar(',');
+putchar(' ');
 }
 }
 
 return (0);
 }
+
 
