af354df627851318f8dc673849dcaa52d540173c
kemboi-73 <kemboimichael52@gmail.com>
2023-02-23 09:18:11 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@
  */
 void print_line(int n)
 {
-    if (n <= 0) {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0) {
+	_putchar('\n');
+	return;
+	}
 
-    while (n--) {
-        _putchar('_');
-    }
-    _putchar('\n');
+	while (n--) {
+	_putchar('_');
+	}
+	_putchar('\n');
 }
 
