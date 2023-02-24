a0c4f627ca780dd96511459bfd40c3a8e4f0d5da
kemboi-73 <kemboimichael52@gmail.com>
2023-02-24 12:17:59 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,44 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- *
- * Description: This C program is an implementation of the classic "FizzBuzz"
- * programming challenge. The program prints the numbers from 1 to 100, but for
- * multiples of three, it prints "Fizz" instead of the number
- * , and for multiples
- * of five, it prints "Buzz". If a number is a multiple of both three and five,
- * it prints "FizzBuzz". The program uses a for loop to iterate through the
- * numbers and if-else statements to determine whether each number meets the
- * criteria for printing "Fizz", "Buzz", or "FizzBuzz".
- *
+ * main - prints the numbers from 1 to 100
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5  multiples print FizzBuzz instead of the number
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
-	if ((i % 3 == 0) && (i % 5 == 0))
-	{
-	printf("FizzBuzz ");
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
+		else if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printf("%d ", i);
 	}
-	else if (i % 3 == 0)
-	{
-	printf("Fizz ");
-	}
-	else if (i % 5 == 0)
-	{
-	printf("Buzz ");
-	}
-	else
-	{
-		printf("%d ", i);
-	}
-}
 	printf("\n");
-
 	return (0);
 }
-
