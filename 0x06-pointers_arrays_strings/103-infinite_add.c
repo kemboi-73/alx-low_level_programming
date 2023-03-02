0574cbf008d8d606cc20cb63dd6e353d6d4740bb
kemboi-73 <kemboimichael52@gmail.com>
2023-03-02 12:03:47 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,52 +1,50 @@
 #include "main.h"
-#include<stdio.h>
-
+#include <stdio.h>
 /**
  * infinite_add - adds two numbers
- * @n1: first number.
- * @n2: Second number
+ * @n1: number one.
+ * @n2: number two.
  * @r: buffer that the function will use to store the result.
- * @size_r: buffer size.
- * Return: the ponter to dest.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int c1 = 0, c2 = 0, m, n, p1, p2, add = 0;
 
 	while (*(n1 + c1) != '\0')
 		c1++;
 	while (*(n2 + c2) != '\0')
 		c2++;
 	if (c1 >= c2)
-		bg = c1;
+		n = c1;
 	else
-		bg = c2;
-	if (size_r <= bg + 1)
+		n = c2;
+	if (size_r <= n + 1)
 		return (0);
-	r[bg + 1] = '\0';
+	r[n + 1] = '\0';
 	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	p1 = *(n1 + c1) - 48, p2 = *(n2 + c2) - 48;
+	while (n >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
+		m = p1 + p2 + add;
+		if (m >= 10)
+			add = m / 10;
 		else
 			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
+		if (m > 0)
+			*(r + n) = (m % 10) + 48;
 		else
-			*(r + bg) = '0';
+			*(r + n) = '0';
 		if (c1 > 0)
-			c2--, dr1 = *(n1 + c1) - 48;
+			c1--, p1 = *(n1 + c1) - 48;
 		else
-			dr1 = 0;
+			p1 = 0;
 		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
+			c2--, p2 = *(n2 + c2) - 48;
 		else
-			dr2 = 0;
-		bg--, size_r--;
+			p2 = 0;
+		n--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
