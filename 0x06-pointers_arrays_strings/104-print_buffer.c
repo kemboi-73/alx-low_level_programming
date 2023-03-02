079cb8f3f5373e2d8bf5003fd81ffc8271a478d4
kemboi-73 <kemboimichael52@gmail.com>
2023-03-02 12:06:13 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,44 +1,42 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_buffer - prints a buffer
  * @b: buffer.
  * @size: size of buffer.
- * Return: no rteurn.
+ * Return: void.
  */
 void print_buffer(char *b, int size)
 {
-	int j, k, l;
+	int f, g, h;
 
 	if (size <= 0)
 		printf("\n");
 	else
 	{
-		for (j = 0; j < size; j += 10)
+		for (f = 0; f < size; f += 10)
 		{
-			printf("%.8x:", j);
-			for (k = j; k < j + 10; k++)
-			{
-				if (k % 2 == 0)
-					printf(" ");
-				if (k < size)
-					printf("%.2x", *(b + k));
-				else
-					printf(" ");
-			}
+			printf("%.8x:", f);
+				for (g = f; g < f + 10; g++)
+				{
+					if (g % 2 == 0)
+						printf(" ");
+					if (g < size)
+						printf("%.2x", *(b + g));
+					else
+						printf("  ");
+				}
 			printf(" ");
-			for (l = j; l < j + 10; l++)
+			for (h = f; h < f + 10; h++)
 			{
-				if (l >= size)
+				if (h >= size)
 					break;
-				if (*(b + 1) < 32 || *(b + 1) > 126)
+				if (*(b + h) < 32 || *(b + h) > 126)
 					printf("%c", '.');
 				else
-					printf("%c", *(b + 1));
+					printf("%c", *(b + h));
 			}
 			printf("\n");
 		}
 	}
 }
-
