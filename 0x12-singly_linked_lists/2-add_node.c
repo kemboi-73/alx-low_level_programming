7c2af9aa645d5acc2e87f830dfb87b4d02427d07
kemboi-73 <kemboimichael52@gmail.com>
2023-04-05 10:10:26 -0700

    function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <lists.h>
+#include "lists.h"
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the head of the list
