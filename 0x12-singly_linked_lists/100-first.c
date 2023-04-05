ae9e405585a703a08e5d4124d52c76e4077c6b85
kemboi-73 <kemboimichael52@gmail.com>
2023-04-05 10:46:12 -0700

    a function that prints before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/**
- * hare_tortoise - function that prints somthing before
- * the main function is executed.
- */
-void hare_tortoise(void) __attribute__ ((constructor));
 
-void hare_tortoise(void)
+void print_before_main(void) __attribute__ ((constructor));
+
+/**
+ * print_before_main - Prints a message before the main function is executed.
+ */
+void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
 
