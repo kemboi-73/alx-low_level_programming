fab7b80133b0f802a1093efd4a6581ba486ccac5
kemboi-73 <kemboimichael52@gmail.com>
2023-04-05 04:44:54 -0700

    headerfile

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints all elements in a list
+ * @h: list to print
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+
+{
+	int nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d", h->n);
+		h = h->next;
+		printf("\n");
+	}
+	return (nodes);
+}
+
