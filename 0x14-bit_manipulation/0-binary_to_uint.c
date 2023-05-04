15a0398f0a2d72c350d27acdd6fb76534e487c0b
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2023-05-04 20:30:34 +0300

    Add files via upload
    Bit manipulation in C involves performing operations on individual bits or groups of bits within a binary number representation, which can be useful in various fields such as low-level programming, cryptography, and image processing.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0 if there is one or more chars
+ * in the string b that is not 0 or 1, or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result = 0;
+	unsigned int bit_value = 1;
+	int i;
+
+	if (b == NULL)
+	return (0);
+
+for (i = strlen(b) - 1; i >= 0; i--)
+	{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+
+if (b[i] == '1')
+result += bit_value;
+
+	bit_value *= 2;
+	}
+
+return (result);
+}
+
