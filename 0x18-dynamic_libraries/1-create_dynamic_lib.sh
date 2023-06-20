ec8f7cab831a01836a9f9526eb71a9bc57e1b4d0
kemboi-73 <kemboimichael52@gmail.com>
2023-06-20 10:37:16 -0700

    a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -Wall -Wextra -Werror -pedantic -c -fpic *.c
+gcc -shared -o liball.so *.o
